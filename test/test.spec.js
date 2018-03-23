cb820846aaa53a3c95b03e7afab119b655bc4633
Iain McDonald <iain.mcdonald@wipro.com>
2018-03-23 18:43:58 +0000

    remove happy path test

--- test/test.spec.js
+++ test/test.spec.js
@@ -6,22 +6,8 @@ const INSERT_BLOB = `
 `;
 
 describe('ibm_db memory issue', () => {
-  describe('when attempting to insert data', () => {
-    let val;
-    beforeEach(() => {
-      val = 'this is content';
-    });
 
-    it('happily inserts', async () => {
-      const insertVal = {
-        DataType: 'BLOB',
-        Data: val
-      };
-      await data.execute(INSERT_BLOB, [insertVal]);
-    });
-  });
-
-  describe('when attempting to insert the same data in base64 format', () => {
+  describe('when attempting to insert the data in base64 format', () => {
     let val;
     beforeEach(() => {
       val = 'dGhlIGZpbGUgY29udGVudA==';
