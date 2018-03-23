4db48c7685a5c0d2a85410c350b47c3200ca734b
Iain McDonald <iain.mcdonald@wipro.com>
2018-03-23 19:05:10 +0000

    reference images from the docker hub registry

--- db/start.sh
+++ db/start.sh
@@ -3,7 +3,7 @@ COMPOSE_NAME=ibm_db_issue
 CURR_DIR=$(pwd)
 THIS_DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 cd $THIS_DIR
-docker-compose -p $COMPOSE_NAME build --no-cache && docker-compose -p $COMPOSE_NAME up -d
+docker-compose -p $COMPOSE_NAME up -d --build
 POLL_DB_EXIT=`docker wait ${COMPOSE_NAME}_poll_db_1`
 docker-compose -p $COMPOSE_NAME logs poll_db
 cd $CURR_DIR
